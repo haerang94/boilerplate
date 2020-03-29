402938f6c57cfcdc1ecc61d71433f35eb1e181ee
haerang <gofkdvjvl@naver.com>
2020-03-30 01:23:58 +0900

    로구아웃 기능 구현

--- models/User.js
+++ models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -32,11 +33,11 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 });
-//비밀 번호를 암호화 시킨다
+
 userSchema.pre("save", function(next) {
   var user = this;
-
   if (user.isModified("password")) {
+    //비밀번호를 암호화 시킨다.
     bcrypt.genSalt(saltRounds, function(err, salt) {
       if (err) return next(err);
 
@@ -49,9 +50,10 @@ userSchema.pre("save", function(next) {
   } else {
     next();
   }
-}); // 유저정보를 저장하기 전에 처리하는 작업
+});
 
 userSchema.methods.comparePassword = function(plainPassword, cb) {
+  //plainPassword 1234567    암호회된 비밀번호 $2b$10$l492vQ0M4s9YUBfwYkkaZOgWHExahjWC
   bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
     if (err) return cb(err);
     cb(null, isMatch);
@@ -60,13 +62,32 @@ userSchema.methods.comparePassword = function(plainPassword, cb) {
 
 userSchema.methods.generateToken = function(cb) {
   var user = this;
-  //jsonwebtoken을 이용해서 token을 생성하기
+  // console.log('user._id', user._id)
+
+  // jsonwebtoken을 이용해서 token을 생성하기
   var token = jwt.sign(user._id.toHexString(), "secretToken");
-  //   user._id + "secretToken" = token;
+  // user._id + 'secretToken' = token
+  // ->
+  // 'secretToken' -> user._id
+
   user.token = token;
-  user.save((err, user) => {
+  user.save(function(err, user) {
     if (err) return cb(err);
     cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function(token, cb) {
+  var user = this;
+  // user._id + ''  = token
+  //토큰을 decode 한다.
+  jwt.verify(token, "secretToken", function(err, decoded) {
+    //유저 아이디를 이용해서 유저를 찾은 다음에
+    //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+    user.findOne({ _id: decoded, token: token }, function(err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
   });
 };
 
