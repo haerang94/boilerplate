89a500cd6dc134e9b12c5786e0b98364c4d0d6da
haerang <gofkdvjvl@naver.com>
2020-04-01 01:45:51 +0900

    login Page done

--- client/src/index.js
+++ client/src/index.js
@@ -2,25 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import {Provider} from 'react-redux';
+import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import "antd/dist/antd.css";
-import {applyMiddleWare} from 'redux';
-import promiseMiddleware from 'redux-promise';
-import ReduxThunk from 'redux-thunk';
-import Reducer from './_reducers';
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
 
-const createStoreWithMiddleware=applyMiddleware(promiseMiddleware,ReduxThunk)(createStore);
-
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 ReactDOM.render(
   <Provider
-  store={createStoreWithMiddleware(Reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )}>
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
     <App />
-  </Provider>
+  </Provider>,
   document.getElementById("root")
 );
-
 
 serviceWorker.unregister();
